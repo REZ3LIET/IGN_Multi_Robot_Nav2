31fa0a700ef9f92360a0b283f9cc7f85901a6c93
REZ3LIET <rz.samar.kale@gmail.com>
2025-02-14 15:48:27 +0000

    Spawning Turtlebot3

--- multi_robot_navigation/launch/robot.launch.py
+++ multi_robot_navigation/launch/robot.launch.py
@@ -1,31 +1,47 @@
+import os
+import xacro
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, PathJoinSubstitution
-from launch.substitutions.launch_configuration import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node, PushRosNamespace
 
-from launch_ros.actions import Node
-
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']   # waffle
 
 def generate_launch_description():
-    declare_namespace_cmd = DeclareLaunchArgument('namespace', default_value='', description='Robot namespace')
+    namespace = "tb1"
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    world_name = LaunchConfiguration('world_name', default='turtlebot3_world')
 
-    pkg_turtlebot4_description = get_package_share_directory('turtlebot4_description')
-    xacro_file = PathJoinSubstitution([pkg_turtlebot4_description, 'urdf/standard/turtlebot4.urdf.xacro'])
-    namespace = LaunchConfiguration('namespace')
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3'), 'launch')
+    new_file_dir = os.path.join(get_package_share_directory('multi_robot_navigation'), 'launch')
 
+    ign_resource_path = SetEnvironmentVariable(
+        name='IGN_GAZEBO_RESOURCE_PATH',value=[
+        os.path.join("/opt/ros/humble", "share"),
+        ":" +
+        os.path.join(get_package_share_directory('turtlebot3'), "models")])
+
+    # Robot state publisher
+    sdf = os.path.join(
+        get_package_share_directory('turtlebot3'),
+        'models', 'turtlebot3', 'model.sdf')
+
+    doc = xacro.parse(open(sdf))
+    xacro.process_doc(doc)
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
+        namespace=namespace,
         output='screen',
         parameters=[
-            {'use_sim_time': True},
-            {'robot_description': Command([
-                'xacro', ' ', xacro_file, ' ',
-                'gazebo:=ignition', ' ',
-                'namespace:=', namespace])},
+            {
+                'use_sim_time': use_sim_time,
+                'robot_description': doc.toxml()
+            }
         ],
         remappings=[
             ('/tf', 'tf'),
@@ -33,22 +49,125 @@ def generate_launch_description():
         ]
     )
 
-    joint_state_publisher = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
+    # Spawn robot
+    ignition_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
         output='screen',
-        parameters=[{'use_sim_time': True}],
+        arguments=['-entity', TURTLEBOT3_MODEL,
+                   '-name', TURTLEBOT3_MODEL,
+                   '-file', PathJoinSubstitution([
+                        get_package_share_directory('turtlebot3'),
+                        "models", "turtlebot3", "model.sdf"]),
+                   '-allow_renaming', 'true',
+                   '-x', '-2.0',
+                   '-y', '-0.5',
+                   '-z', '0.01'],
+        )
+    
+    # Spawn world
+    ignition_spawn_world = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-file', PathJoinSubstitution([
+                        get_package_share_directory('turtlebot3'),
+                        "models", "worlds", "model.sdf"]),
+                   '-allow_renaming', 'false'],
+        )
+
+
+    world_only = os.path.join(get_package_share_directory('turtlebot3'), "models", "worlds", "world_only.sdf")
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        namespace=namespace,
+        parameters=[
+            {
+                'use_sim_time': use_sim_time,
+                'expand_gz_topic_names': True
+            }
+        ],
+        arguments=[
+                # Velocity command (ROS2 -> IGN)
+                'cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',
+                # Odometry (IGN -> ROS2)
+                'odom@nav_msgs/msg/Odometry[ignition.msgs.Odometry',
+                # TF (IGN -> ROS2)
+                'odom/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V',
+                # Clock (IGN -> ROS2)
+                'clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+                # Joint states (IGN -> ROS2)
+                'joint_states@sensor_msgs/msg/JointState[ignition.msgs.Model',
+                # Lidar (IGN -> ROS2)
+                'scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
+                'scan/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
+                # IMU (IGN -> ROS2)
+                'imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
+                # Camera (IGN -> ROS2)
+                'camera/rgb/image_raw@sensor_msgs/msg/Image[ignition.msgs.Image',
+                'camera/rgb/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+                ],
         remappings=[
-            ('/tf', 'tf'),
-            ('/tf_static', 'tf_static')
-        ]
+            ("odom/tf", "tf")
+        ],
+        output='screen'
     )
 
-    # Define LaunchDescription variable
-    ld = LaunchDescription()
-    # Add nodes to LaunchDescription
-    ld.add_action(declare_namespace_cmd)
-    ld.add_action(robot_state_publisher)
-    ld.add_action(joint_state_publisher)
-    return ld
+    # Nav2 Bringup
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('turtlebot3'),
+            'maps',
+            'turtlebot3_world.yaml'))
+
+    param_file_name = TURTLEBOT3_MODEL + '.yaml'
+    param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('turtlebot3'),
+            'params',
+            param_file_name))
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    nav2_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(nav2_launch_file_dir, 'bringup_launch.py')),
+        launch_arguments={
+            'namespace': namespace,
+            'slam': 'False',
+            'map': map_dir,
+            'use_sim_time': use_sim_time,
+            'params_file': param_dir,
+            'autostart': 'True',
+            'use_composition': 'True',
+            'use_respawn': 'False',
+        }.items(),
+    )
+
+    return LaunchDescription([
+        ign_resource_path,
+        ignition_spawn_entity,
+        ignition_spawn_world,
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('ros_gz_sim'),
+                              'launch', 'gz_sim.launch.py')]),
+            launch_arguments=[('ign_args', [' -r -v1 ' +
+                              world_only
+                             ])]),
+                             
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='If true, use simulated clock'),
+
+        DeclareLaunchArgument(
+            'world_name',
+            default_value=world_name,
+            description='World name'),
+
+        robot_state_publisher,
+        nav2_bringup,
+        bridge,
+    ])
