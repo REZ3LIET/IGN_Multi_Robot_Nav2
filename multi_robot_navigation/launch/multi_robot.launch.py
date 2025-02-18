759250660c00e133a85e2f3314716977e656b545
REZ3LIET <rz.samar.kale@gmail.com>
2025-02-18 02:54:06 +0000

    Final modifications to implement multi robot navigation

--- multi_robot_navigation/launch/multi_robot.launch.py
+++ multi_robot_navigation/launch/multi_robot.launch.py
@@ -26,28 +26,58 @@ from launch_ros.actions import Node
 from launch.event_handlers import OnProcessExit
 from launch.conditions import IfCondition
 import xacro
+import yaml
 
+def replace_namespace(d, old_str, new_str):
+    if isinstance(d, dict):
+        for key, value in d.items():
+            d[key] = replace_namespace(value, old_str, new_str)
+    elif isinstance(d, list):
+        for i in range(len(d)):
+            d[i] = replace_namespace(d[i], old_str, new_str)
+    elif isinstance(d, str):
+        return d.replace(old_str, new_str)
+    return d
+
+def replace_namespace_in_params(yaml_file_path, namespace):
+    if not namespace.startswith('/'):
+        namespace = '/' + namespace
+
+    with open(yaml_file_path, 'r') as file:
+        data = yaml.safe_load(file)
+    
+    # Replace namespace with 'namespace'
+    data = replace_namespace(data, 'namespace', namespace)
+
+    # Convert the modified data back to YAML format
+    modified_yaml = yaml.dump(data, default_flow_style=False)
+
+    # Write the modified YAML to the output file
+    path = f"/tmp{namespace}.yaml"
+    with open(path, 'w') as file:
+        file.write(modified_yaml)
+
+    print(f"Modified YAML has been saved to {path}")
+    return path
 
 def generate_launch_description():
     ld = LaunchDescription()
 
     # Names and poses of the robots
     robots = [
-        {'name': 'tb1', 'x_pose': '-1.5', 'y_pose': '-0.5', 'z_pose': 0.01},
-        # {'name': 'tb2', 'x_pose': '-1.5', 'y_pose': '0.5', 'z_pose': 0.01},
-        # {'name': 'tb3', 'x_pose': '1.5', 'y_pose': '-0.5', 'z_pose': 0.01},
-        # {'name': 'tb4', 'x_pose': '1.5', 'y_pose': '0.5', 'z_pose': 0.01},
+        {'name': 'tb1', 'x_pose': '-1.5', 'y_pose': '-0.5', 'z_pose': '0.01'},
+        {'name': 'tb2', 'x_pose': '-1.5', 'y_pose': '0.5', 'z_pose': '0.01'},
+        # {'name': 'tb3', 'x_pose': '1.5', 'y_pose': '-0.5', 'z_pose': '0.01'},
+        # {'name': 'tb4', 'x_pose': '1.5', 'y_pose': '0.5', 'z_pose': '0.01'},
         # ...
         # ...
         ]
-
-    TURTLEBOT3_MODEL = 'waffle'
 
     ign_resource_path = SetEnvironmentVariable(
         name='IGN_GAZEBO_RESOURCE_PATH',value=[
         os.path.join("/opt/ros/humble", "share"),
         ":" +
-        os.path.join(get_package_share_directory('turtlebot3'), "models")])
+        os.path.join(get_package_share_directory('multi_robot_navigation'), "models")])
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     declare_use_sim_time = DeclareLaunchArgument(
@@ -58,11 +88,11 @@ def generate_launch_description():
     nav_launch_dir = os.path.join(turtlebot3_multi_robot, 'launch', 'nav2_mod')
 
     sdf = os.path.join(
-        get_package_share_directory('turtlebot3'),
+        get_package_share_directory('multi_robot_navigation'),
         'models', 'turtlebot3', 'model.sdf')
 
 
-    world_only = os.path.join(get_package_share_directory('turtlebot3'), "models", "worlds", "world_only.sdf")
+    world_only = os.path.join(get_package_share_directory('multi_robot_navigation'), "models", "worlds", "world_only.sdf")
     ign_gz = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 [os.path.join(get_package_share_directory('ros_gz_sim'),
@@ -76,20 +106,14 @@ def generate_launch_description():
         executable='create',
         output='screen',
         arguments=['-file', PathJoinSubstitution([
-                        get_package_share_directory('turtlebot3'),
+                        get_package_share_directory('multi_robot_navigation'),
                         "models", "worlds", "model.sdf"]),
                    '-allow_renaming', 'false'],
         )
 
-    params_file = LaunchConfiguration('nav_params_file')
-    declare_params_file_cmd = DeclareLaunchArgument(
-        'nav_params_file',
-        default_value=os.path.join(turtlebot3_multi_robot, 'params', 'nav2_params.yaml'),
-        description='Full path to the ROS2 parameters file to use for all launched nodes')
-    
+    params_file = os.path.join(turtlebot3_multi_robot, 'params', 'nav2_params.yaml')    
      
     ld.add_action(declare_use_sim_time)
-    ld.add_action(declare_params_file_cmd)
     ld.add_action(ign_resource_path)
     ld.add_action(ign_gz)
     ld.add_action(ignition_spawn_world)
@@ -112,26 +136,7 @@ def generate_launch_description():
         ]
     )
 
-    map_server=Node(package='nav2_map_server',
-        executable='map_server',
-        name='map_server',
-        output='screen',
-        parameters=[{'yaml_filename': yaml_filename,
-                     },],
-        remappings=remappings)
-
-    map_server_lifecyle=Node(package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_map_server',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time},
-                        {'autostart': True},
-                        {'node_names': ['map_server']}])
-
-
     ld.add_action(clock_bridge)
-    # ld.add_action(map_server)
-    # ld.add_action(map_server_lifecyle)
 
     ######################
 
@@ -168,10 +173,12 @@ def generate_launch_description():
         arguments=['-topic', f"{namespace}/robot_description",
                    '-name', robot['name'],
                    '-robot_namespace', namespace,
-                   '-x', '-2.0',
-                   '-y', '-0.5',
-                   '-z', '0.01'],
+                   '-x', robot['x_pose'],
+                   '-y', robot['y_pose'],
+                   '-z', robot['z_pose']],
         )
+
+        namespaced_param_file = replace_namespace_in_params(params_file, namespace)
 
         bringup_cmd = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
@@ -182,7 +189,7 @@ def generate_launch_description():
                         'use_namespace': 'True',
                         'map': yaml_filename,
                         'map_server': 'True',
-                        'params_file': params_file,
+                        'params_file': namespaced_param_file,
                         'default_bt_xml_filename': os.path.join(
                             get_package_share_directory('nav2_bt_navigator'),
                             'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
@@ -207,23 +214,12 @@ def generate_launch_description():
             output='screen'
         )
 
-        static_transform_publisher = Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            name="map_to_odom",
-            namespace=namespace,
-            output="screen",
-            arguments=['0', '0', '0', '0', '0', '0', "map", "odom"],
-            remappings=remappings
-        )
-
         if last_action is None:
             # Call add_action directly for the first robot to facilitate chain instantiation via RegisterEventHandler
             ld.add_action(turtlebot_state_publisher)
             ld.add_action(spawn_turtlebot3_burger)
             ld.add_action(bringup_cmd)
             ld.add_action(bridge)
-            ld.add_action(static_transform_publisher)
 
         else:
             # Use RegisterEventHandler to ensure next robot creation happens only after the previous one is completed.
@@ -235,8 +231,7 @@ def generate_launch_description():
                         spawn_turtlebot3_burger,
                         turtlebot_state_publisher,
                         bringup_cmd,
-                        bridge,
-                        static_transform_publisher
+                        bridge
                     ],
                 )
             )
@@ -287,7 +282,6 @@ def generate_launch_description():
         last_action = initial_pose_cmd
 
         ld.add_action(post_spawn_event)
-        ld.add_action(declare_params_file_cmd)
     ######################
 
     return ld
